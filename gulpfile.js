33c7df75aace14a063f92700d2f8eec93dfdc289
lucas-paulo-pires <lucaspaulo.pires@gmail.com>
2016-11-24 13:53:48 -0200

    commit gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
-gulp.task('default', function(){
-	console.log('elf');
-})
+// task para o sass
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+// task para watch
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass']);
+});
+
+// task default gulp
+gulp.task('default', ['sass', 'watch']);
